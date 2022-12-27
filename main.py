0a52d0d98d17976695f78c1975290bf3a14e0d3d
Otniel <otnielsilvag4@gmail.com>
2022-12-27 18:17:24 -0300

    alterando o retorno

--- main.py
+++ main.py
@@ -7,50 +7,65 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 class Mensagem:
-  pessoa_nome:str
-  id_pessoa:str
-  id_mensagem:str
-  texto:str
+    pessoa_nome:str
+    id_pessoa:str
+    id_mensagem:str
+    texto:str
 
-  def __init__(self, pessoa_nome, id_pessoa, id_mensagem, texto):
-    self.pessoa_nome=pessoa_nome
-    self.id_pessoa=id_pessoa
-    self.id_mensagem=id_mensagem
-    self.texto=texto
+    def __init__(self, pessoa_nome, id_pessoa, id_mensagem, texto):
+        self.pessoa_nome = pessoa_nome
+        self.id_pessoa = id_pessoa
+        self.id_mensagem = id_mensagem
+        self.texto = texto
 
-  @staticmethod
-  def from_dict(json):
-    return Mensagem(
-      pessoa_nome=json.get("pessoa_nome"),
-      id_pessoa=json.get("id_pessoa"),
-      id_mensagem=json.get("id_mensagem"),
-      texto=json.get("texto")
-    )
+    def __repr__(self):
+        return repr(f"<{self.pessoa_nome} -> {self.texto}>")
 
-  def to_json(self):
-    return {
-      "pessoa_nome": self.pessoa_nome,
-      "id_pessoa": self.id_pessoa,
-      "id_mensagem": self.id_mensagem,
-      "texto": self.texto
-    }
+    @staticmethod
+    def from_dict(json):
+        return Mensagem(
+          pessoa_nome=json.get("pessoa_nome"),
+          id_pessoa=json.get("id_pessoa"),
+          id_mensagem=json.get("id_mensagem"),
+          texto=json.get("texto")
+        )
+
+    def to_json(self):
+        return {
+          "pessoa_nome": self.pessoa_nome,
+          "id_pessoa": self.id_pessoa,
+          "id_mensagem": self.id_mensagem,
+          "texto": self.texto
+        }
 
 
 class Conversa:
-  pessoa1:int
-  pessoa2:int
-  conversa:list[Mensagem] = []
+    pessoa1:int
+    pessoa2:int
+    conversa:list[Mensagem] = []
+
+    @staticmethod
+    def retorna_conversa():
+        return [{
+            "pessoa_nome": c.pessoa_nome,
+            "id_pessoa": c.id_pessoa,
+            "id_mensagem": c.id_mensagem,
+            "texto": c.texto
+        } for c in Conversa.conversa]
 
 # Define uma ação para ser executada quando um evento 'message' é recebido
 @socketio.on('message')
 def handle_message(data):
     print(data)
     mensagem = Mensagem.from_dict(data.get("mensagem"))
-    pessoa = data.get("pessoa")
     Conversa.conversa.append(mensagem)
-    if pessoa != mensagem.id_pessoa:
-      resposta = mensagem.to_json()
-      emit('getMessage', resposta, json=True)
+    resposta = mensagem.to_json()
+    emit('getMessage', resposta, json=True, broadcast=True)
+
+@socketio.on('todaConversa')
+def retorn_toda_conversa():
+    print(Conversa.retorna_conversa())
+    emit("pegaConversa", Conversa.retorna_conversa(), json=True)
 
 @socketio.on('json')
 def handle_json(json):
@@ -59,4 +74,15 @@ def handle_json(json):
 
 
 if __name__ == '__main__':
-  socketio.run(app)
+    socketio.run(app)
+
+
+# @socketio.on('message')
+# def handle_message(data):
+#     print(data)
+#     mensagem = Mensagem.from_dict(data.get("mensagem"))
+#     pessoa = data.get("pessoa")
+#     Conversa.conversa.append(mensagem)
+#     print(Conversa.conversa)
+#     resposta = Conversa.retorna_conversa()
+#     emit('getMessage', resposta, json=True, broadcast=True)
